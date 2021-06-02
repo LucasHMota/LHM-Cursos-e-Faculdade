d13c3a9c66b3201cca5f248215fb924ed1541ac7
LucasHM <58492603+LucasHMota@users.noreply.github.com>
2021-06-02 09:59:29 -0300

    Add files via upload

---
+++ salvar-dados.php
@@ -0,0 +1,30 @@
+<!DOCTYPE HTML>
+<!--
+	Phantom by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+		<title>Faça sua consulta conosco</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
+	</head>
+	<body class="is-preload">
+		<!-- Wrapper -->
+        <div id="wrapper">
+							<!-- Logo -->
+								<a href="planomedico.php" class="logo">
+									<span class="symbol"><center><img src="unimed.png" width="500" height="250" alt="" /></span><span class="title"><center><h2>Dados gravados com sucesso!</h2></span>
+								</a>
+						</div>
+					</header>
+					<center><input type="button" onClick="javascript: location.href='planomedico.php'" value="Voltar para página principal" class=primary>
+					<br><br><br><br><br><h3>Desenvolvido por: Lucas Henrique Mota - ADS 4.</h3>
+				<!-- Main -->
+					<div id="main">
+						<div class="inner">
+</body>
+</html>
